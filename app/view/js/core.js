17d3070dbdf07afd168a0784b8c4c498409e1a5c
MuzzamilQureshi007 <62890150+MuzzamilQureshi007@users.noreply.github.com>
2020-12-21 14:20:23 +0500

    header navigation set

--- app/view/js/core.js
+++ app/view/js/core.js
@@ -50,3 +50,18 @@ $(window).scroll(function(){
         $('.section-Manage').removeClass('scrollEffect2')
     }
 })
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
