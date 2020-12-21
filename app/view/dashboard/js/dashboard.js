8fd1018ee3fe3017ba3cb423930e21f9a5269288
MuzzamilQureshi007 <62890150+MuzzamilQureshi007@users.noreply.github.com>
2020-12-21 21:54:18 +0500

    sidebar update

--- app/view/dashboard/js/dashboard.js
+++ app/view/dashboard/js/dashboard.js
@@ -145,3 +145,8 @@ $('#password, #confirmPassword').on('keyup', function () {
 
 
 
+$('.sub-menu ul').hide();
+$(".sub-menu a").click(function () {
+	$(this).parent(".sub-menu").children("ul").slideToggle("100");
+	$(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
+});
--- app/view/js/core.js
+++ app/view/js/core.js
@@ -85,3 +85,8 @@ function openCloseDropdown(event) {
 	}
 }
 
+$('.sub-menu ul').hide();
+$(".sub-menu a").click(function () {
+	$(this).parent(".sub-menu").children("ul").slideToggle("100");
+	$(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
+});
